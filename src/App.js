e18bd3fb0d01f35bfbc3089592c89aa2156c0a00
Slameich-web <SlameACER@mail.ru>
2025-03-14 03:39:13 +0300

    test

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@
  * Renders global state providers.
  */
 
-import { BrowserRouter as Router } from 'react-router-dom';
-import Routes from 'routes';
-import { ApplicationState } from 'containers/application/state';
-import 'utils/localization';
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "routes";
+import { ApplicationState } from "containers/application/state";
+import "utils/localization";
+import { useEffect } from "react";
 
 const App = () => {
+  const test = async () => {
+    const app_cache = await caches?.open("app");
+    console.log("app_cache", app_cache);
+    const response = await app_cache?.match("/path/to/your/data");
+    console.log("response", response);
+  };
+  useEffect(() => {
+    test();
+  });
   return (
     <ApplicationState.Provider>
       <Router>
