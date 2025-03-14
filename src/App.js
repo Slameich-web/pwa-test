bb3351ae0fb9892a078844cb6ff4097fee3db9a7
Slameich-web <SlameACER@mail.ru>
2025-03-14 03:54:09 +0300

    test

--- src/App.js
+++ src/App.js
@@ -14,7 +14,19 @@ import { useEffect, useState } from "react";
 const App = () => {
   const [state1, setState1] = useState();
   const [state2, setState2] = useState();
+  const [state3, setState3] = useState();
+  const [state4, setState4] = useState();
 
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      if (registrations.length) {
+        setState3("Service Worker установлен");
+        setState4(registrations);
+      } else {
+        setState3("Service Worker не установлен");
+      }
+    });
+  }
   const test = async () => {
     const app_cache = await caches?.open();
     console.log("app_cache", app_cache);
@@ -34,6 +46,13 @@ const App = () => {
       </Router>
       <div>state1 {state1}</div>
       <div>state2 {state2}</div>
+      <div>state3 {state3}</div>
+      <div>
+        state4{" "}
+        {state4.map((i) => (
+          <div>{JSON.stringify(i)}</div>
+        ))}
+      </div>
     </ApplicationState.Provider>
   );
 };
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,27 +1,63 @@
-import Logo from 'components/Logo';
-import { useTranslation } from 'react-i18next';
-import { useDisplayType } from 'utils/display';
-import { DisplayTypes } from 'utils/display/types';
-import { useComponentContainer } from './container';
-import styles from './home.module.css';
+import Logo from "components/Logo";
+import { useTranslation } from "react-i18next";
+import { useDisplayType } from "utils/display";
+import { DisplayTypes } from "utils/display/types";
+import { useComponentContainer } from "./container";
+import styles from "./home.module.css";
+import { useEffect, useState } from "react";
 
 function Home() {
   const { t } = useTranslation();
   const display = useDisplayType();
   const Component = useComponentContainer();
+  const [state1, setState1] = useState<any>();
+  const [state2, setState2] = useState<any>();
+  const [state3, setState3] = useState("");
+  const [state4, setState4] = useState<any>();
 
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      if (registrations.length) {
+        setState3("Service Worker установлен");
+        setState4(registrations);
+      } else {
+        setState3("Service Worker не установлен");
+      }
+    });
+  }
+  const test = async () => {
+    const app_cache = await caches?.open("app");
+    console.log("app_cache", app_cache);
+    setState1(JSON.stringify(app_cache));
+    const response = await app_cache?.match("/path/to/your/data");
+    console.log("response", response);
+    setState2(JSON.stringify(response));
+  };
+  useEffect(() => {
+    test();
+    console.log("test");
+  });
   return (
     <div className={styles.app}>
       <header className={styles.appHeader}>
         <Logo />
-        <p className={styles.welcomeText}>{t('welcome')}</p>
+        <p className={styles.welcomeText}>{t("welcome")}</p>
         {display === DisplayTypes.DESKTOP ? <p>Desktop</p> : <p>Mobile</p>}
         {Component.State.todos.map((t) => (
-          <div className={t.completed ? styles.completed : ''} key={t.id}>
+          <div className={t.completed ? styles.completed : ""} key={t.id}>
             {t.title}
           </div>
         ))}
       </header>
+      <div>state1 {state1}</div>
+      <div>state2 {state2}</div>
+      <div>state3 {state3}</div>
+      <div>
+        state4{" "}
+        {state4.map((i: any) => (
+          <div>{JSON.stringify(i)}</div>
+        ))}
+      </div>
     </div>
   );
 }
